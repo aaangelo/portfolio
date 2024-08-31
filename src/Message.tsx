036b03f65c6eaef656cef806b59099a93d996b03
aaangelo <angelofc97@gmail.com>
2024-08-31 20:42:07 +0100

    delete everything and add game

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    const name: string = "Angelo";
+    return <iframe frameBorder="0" src="https://itch.io/embed-upload/9122319?color=C0D470" width="1280" height="740"><a href="https://angeloangelo.itch.io/map-game">Play Map Game on itch.io</a></iframe>;
+}
+
+export default Message;
