e11bd3c10a0e4b2c78398a4b31d64e30942090f2
aaangelo <angelofc97@gmail.com>
2025-03-15 10:50:08 +0000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <div>
       <h1>this is where the heading will go</h1>
+      <h2>something else idk</h2>
       <NavBar onSelectItem={handleSelectItem} />
     </div>
   );
